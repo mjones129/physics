ece5c2eeffa03fd61b6406463f22541e7e09165f
Matt Jones <me@mattjones.tech>
2024-09-03 19:49:26 -0400

    add physics materials to ball and floor

--- src/script.js
+++ src/script.js
@@ -39,12 +39,27 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
 
+//materials
+const concreteMaterial = new CANNON.Material("concrete");
+const plasticMaterial = new CANNON.Material("plastic");
+
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+  concreteMaterial,
+  plasticMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7,
+  },
+);
+world.addContactMaterial(concretePlasticContactMaterial);
+
 //sphere
 const sphereShape = new CANNON.Sphere(0.5); //same radius as the THREE.js buffer geometry
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
   shape: sphereShape,
+  material: plasticMaterial,
 });
 world.addBody(sphereBody);
 
@@ -52,6 +67,7 @@ world.addBody(sphereBody);
 const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body();
 floorBody.mass = 0; //this is the default value for mass
+floorBody.material = concreteMaterial;
 floorBody.addShape(floorShape);
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
 world.addBody(floorBody);
