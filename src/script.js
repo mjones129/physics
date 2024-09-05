daea6841ee1fc6421469c8cbe7c8ac10c2b6e895
Matt Jones <me@mattjones.tech>
2024-09-05 03:59:54 -0400

    optimize sphere creation

--- src/script.js
+++ src/script.js
@@ -160,17 +160,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  */
 
 const objectsToUpdate = [];
+const spehereGeometry = new THREE.SphereGeometry(1, 20, 20);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
 
 const createSphere = (radius, position) => {
   //Three Mesh
-  const mesh = new THREE.Mesh(
-    new THREE.SphereGeometry(radius, 20, 20),
-    new THREE.MeshStandardMaterial({
-      metalness: 0.3,
-      roughness: 0.4,
-      envMap: environmentMapTexture,
-    }),
-  );
+  const mesh = new THREE.Mesh(spehereGeometry, sphereMaterial);
+  mesh.scale.set(radius, radius, radius);
   mesh.castShadow = true;
   mesh.position.copy(position);
   scene.add(mesh);
